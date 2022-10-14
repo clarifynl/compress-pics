34a5da4dfcc22c487720649a18ce4a0f0860ca84
Abel Schupp <abel_schupp@hotmail.com>
2022-10-14 17:25:53 +0200

    add hooks

--- src/Compressor.php
+++ src/Compressor.php
@@ -18,9 +18,56 @@ class Compressor
 	{
 		if (self::$instance == null)
 		{
-			self::$instance = new Singleton();
+			self::$instance = new Compressor();
 		}
 
 		return self::$instance;
+	}
+
+	public static function compressImage($path) {
+		if (!file_exists($path)) {
+			error_log($path, ' is not a path.');
+
+			return null;
+		}
+
+		$instance = new PNGQuant();
+		$path_info = pathinfo($path);
+		$dir = $path_info['dirname'];
+		$filename = $path_info['filename'];
+
+
+		$exit_code = $instance->setImage($path)
+			->setOutputImage($path)
+			->overwriteExistingFile()
+			->setQuality(50,80)
+			->execute();
+
+		if ($exit_code != 0){
+			error_log("Something went wrong (status code $exit_code)  with description: ". $instance->getErrorTable()[(string) $exit_code]);
+		}
+	}
+
+	public static function onSaveImage($metadata) {
+		$path = get_attached_file($id);
+		$path_info = pathinfo($path);
+		$image_dir = $path_info['dirname'];
+
+		if ($path_info['extension'] == 'png') {
+			self::compressImage($path);
+
+			foreach ($metadata['sizes'] as $size => $value) {
+				$size_path = $image_dir . '/' . $value['file'];
+				self::compressImage($size_path);
+			}
+		}
+
+		return $metadata;
+	}
+
+	public static function onRPSaved ($post_id, $file) {
+		if ($file['mime-type'] == 'image/png') {
+			self::compressImage($file['path']);
+		}
 	}
 }
--- src/Hooks.php
+++ src/Hooks.php
@@ -5,23 +5,10 @@ namespace Abel\CompressPics;
 class Hooks
 {
 	public function __construct() {
-		echo 'hooks started';
-		exit();
-		// add_filter('wp_generate_attachment_metadata', function ($metadata, $id, $context) {
-		// 	$path = get_attached_file($id);
-		// 	$path_info = pathinfo($path);
-		// 	$image_dir = $path_info['dirname'];
+		add_filter('wp_generate_attachment_metadata', ['Abel\CompressPics\Compressor', 'onSaveImage'], 10, 3);
 
-		// 	if ($path_info['extension'] == 'png') {
-		// 		compress_image($path);
-
-		// 		foreach ($metadata['sizes'] as $size => $value) {
-		// 			$size_path = $image_dir . '/' . $value['file'];
-		// 			compress_image($size_path);
-		// 		}
-		// 	}
-
-		// 	return $metadata;
-		// }, 10, 3);
+		if (class_exists('ResponsivePics')) {
+			add_action('responsive_pics_file_saved_local', ['Abel\CompressPics\Compressor', 'onRPicsSaved'], 10, 3);
+		}
 	}
 }
